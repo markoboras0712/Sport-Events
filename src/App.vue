51971e7e6d595704d03b6ee041c0f95c2789ecd3
markoboras0712 <mboras1@etfos.hr>
2021-05-07 10:34:54 +0300

    2-hamburger-drawer-added

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <template>
     <v-app>
-      <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon></v-toolbar-side-icon>
-            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
-            <v-btn flat>Menu</v-btn>
-            <v-spacer></v-spacer>
-            <v-btn flat>SIGN IN</v-btn>
-            <v-btn color="brown lighten-3">JOIN</v-btn>
-        </v-toolbar>
-        <v-content transition="slide-x-transition">
+        <AppNavigation/>
+        <main>
             <router-view></router-view>
-        </v-content>
+        </main>
     </v-app>
 </template>
 
 <script>
+import AppNavigation from './components/Navigation/AppNavigation.vue';
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+      AppNavigation
+    }
 };
 </script>
 
---
+++ src/components/Navigation/AppNavigation.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+   
+    <v-app-bar
+      dark
+      prominent
+      src="https://myredmouse.com/wp-content/uploads/2019/04/Background-Header-1.jpg"
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+        <v-spacer></v-spacer>
+      <v-toolbar-title>SportMeetup</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Account</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppNavigation",
+  data(){
+      return {
+          drawer: false,
+      group: null,
+      }
+  }
+};
+</script>
+
+<style scoped></style>
