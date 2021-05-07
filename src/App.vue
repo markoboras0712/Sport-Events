ec59b34fad8c517dcdcaa85383018a621fe34572
markoboras0712 <mboras1@etfos.hr>
2021-05-07 10:03:49 +0300

    1-starting-navigation

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app>
+      <v-toolbar app color="brown darken-4" dark>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title>{{appTitle}}</v-toolbar-title>
+            <v-btn flat>Menu</v-btn>
+            <v-spacer></v-spacer>
+            <v-btn flat>SIGN IN</v-btn>
+            <v-btn color="brown lighten-3">JOIN</v-btn>
+        </v-toolbar>
+        <v-content transition="slide-x-transition">
+            <router-view></router-view>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+    name: 'App'
+};
+</script>
+
+<style>
+</style>
