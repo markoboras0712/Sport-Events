7a185e85d2b3b0539c88deee749a27779572d3f4
markoboras0712 <mboras1@etfos.hr>
2021-05-07 11:30:42 +0300

    3-navigation-layout-finished

--- src/components/Navigation/AppNavigation.vue
+++ src/components/Navigation/AppNavigation.vue
@@ -1,40 +1,26 @@
 <template>
   <div>
-   
     <v-app-bar
       dark
       prominent
       src="https://myredmouse.com/wp-content/uploads/2019/04/Background-Header-1.jpg"
     >
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-        <v-spacer></v-spacer>
+      <v-spacer></v-spacer>
       <v-toolbar-title>SportMeetup</v-toolbar-title>
     </v-app-bar>
-    <v-navigation-drawer
-      v-model="drawer"
-      absolute
-      temporary
-    >
-      <v-list
-        nav
-        dense
-      >
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
         <v-list-item-group
-          v-model="group"
+          v-for="item in menuItems"
+          :key="item.title"
           active-class="deep-purple--text text--accent-4"
         >
-          <v-list-item>
+          <v-list-item :to="item.link">
             <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
+              <v-icon>{{ item.icon }}</v-icon>
             </v-list-item-icon>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item>
-
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Account</v-list-item-title>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item>
         </v-list-item-group>
       </v-list>
@@ -43,14 +29,32 @@
 </template>
 
 <script>
+import {
+  mdiViewListOutline,
+  mdiMapMarkerPlus,
+  mdiAccount,
+  mdiAccountPlus,
+  mdiLoginVariant,
+} from "@mdi/js";
 export default {
   name: "AppNavigation",
-  data(){
-      return {
-          drawer: false,
+  data() {
+    return {
+      drawer: false,
       group: null,
-      }
-  }
+      menuItems: [
+        { icon: mdiViewListOutline, title: "View Meetups", link: "/meetups" },
+        {
+          icon: mdiMapMarkerPlus,
+          title: "Organize Meetup",
+          link: "/meetup/new",
+        },
+        { icon: mdiAccount, title: "Profile", link: "/profile" },
+        { icon: mdiAccountPlus, title: "Sign up", link: "/signup" },
+        { icon: mdiLoginVariant, title: "Sign in", link: "/signin" },
+      ],
+    };
+  },
 };
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,44 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import Meetups from '@/components/Meetup/Meetups';
+import CreateMeetup from '@/components/Meetup/CreateMeetup';
+import Profile from '@/components/User/Profile';
+import Signup from '@/components/User/Signup';
+import Signin from '@/components/User/Signin';
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Home',
+    component: Home
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/meetups',
+    name: 'Meetups',
+    component: Meetups
   },
+  {
+    path: '/meetup/new',
+    name: 'CreateMeetup',
+    component: CreateMeetup
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    component: Profile
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    component: Signin
+  }
   
 ];
 
